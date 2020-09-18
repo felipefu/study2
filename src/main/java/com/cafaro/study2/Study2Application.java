59f2547dde20df691df7986699c1a7a039e5ef75
Felipe Cafaro <felipecafaro@gmail.com>
2020-09-17 19:30:18 -0700

    format

--- src/main/java/com/cafaro/study2/Study2Application.java
+++ src/main/java/com/cafaro/study2/Study2Application.java
@@ -4,20 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
 
 @SpringBootApplication
 public class Study2Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Study2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Study2Application.class, args);
+    }
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(Study2Application.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Study2Application.class);
+    }
 }
